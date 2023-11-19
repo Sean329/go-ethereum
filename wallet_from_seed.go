69042b3f46aa0fe376818be023a84d42e4f18fdf
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-19 00:01:43 -0800

    added the script of generating a wallet from a seed

---
+++ wallet_from_seed.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mnemonic := os.Getenv("WEB3_PRACTICE_MNEMONIC")
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
+	account, err := wallet.Derive(path, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex()) // 
+
+	path = hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/1")
+	account, err = wallet.Derive(path, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex()) // 
+}
