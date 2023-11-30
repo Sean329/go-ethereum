7d414ad93f1e24c3acf9999ce1c20879cf0d3af2
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-29 20:34:31 -0800

    Finished the script of transfer_eth.go

--- transfer_eth.go
+++ transfer_eth.go
@@ -2,14 +2,76 @@ package main
 
 import (
     "context"
+    "crypto/ecdsa"
     "fmt"
     "log"
-    // "math/big"
+    "math/big"
 	"os"
 
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/crypto"
     "github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 )
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pk := os.Getenv("WEB3_PRACTICE_PRIVATE_KEY")
+	alchemyEthereumGoerliURL:=os.Getenv("ALCHEMY_GOERLI_URL")
+
+	client, err := ethclient.Dial(alchemyEthereumGoerliURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	privateKey, err := crypto.HexToECDSA(pk)
+	if err != nil {
+ 		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+    publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+    if !ok {
+        log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+    }
+
+    fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+    nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	value := big.NewInt(0) // in wei (1 eth)
+    gasLimit := uint64(21000) // in units
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    toAddress := common.HexToAddress("0xeF145f88397f2E5aa64b56Ec49e67e7Ed2644FC8")
+    var data []byte
+    tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.SendTransaction(context.Background(), signedTx)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("tx sent: %s \n", signedTx.Hash().Hex())
+	
+}
