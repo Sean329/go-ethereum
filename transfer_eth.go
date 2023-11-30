ffa3cdd6e223fbddcc01f2060c200f549f7464ee
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-11-29 20:19:20 -0800

    WIP - the script to send ETH

---
+++ transfer_eth.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    // "math/big"
+	"os"
+
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+)
+
