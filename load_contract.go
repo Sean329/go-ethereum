691bfc5e9e8138e207d27c4dccace164becee444
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-12-10 00:20:06 -0800

    added the script load_contract.go

---
+++ load_contract.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "log"
+	"os"
+
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+
+    store "github.com/Sean329/go-ethereum/contracts" // for demo
+)
+
+func main() {
+    err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	alchemyEthereumGoerliURL:=os.Getenv("ALCHEMY_GOERLI_URL")
+
+	client, err := ethclient.Dial(alchemyEthereumGoerliURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    address := common.HexToAddress("0x60193B0F538D02cccA6Ab55F7EbA9d7Cb000C773")
+    instance, err := store.NewStore(address, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("contract is loaded")
+    _ = instance
+}
