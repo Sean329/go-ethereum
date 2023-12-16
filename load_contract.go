244842b900625aa1393141431bdcd30eb9ea1c0a
Xiangyu Xu <xiangyuxu329@gmail.com>
2023-12-15 21:23:26 -0800

    Updated load_contract.go to write to the contract

--- load_contract.go
+++ load_contract.go
@@ -4,9 +4,15 @@ import (
     "fmt"
     "log"
 	"os"
+    "context"
+    "math/big"
+    "crypto/ecdsa"
+    "time"
 
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/joho/godotenv"
 
     store "github.com/Sean329/go-ethereum/contracts" 
@@ -39,4 +45,60 @@ func main() {
     }
 
     fmt.Println(version) // "1.0"
+
+    pk := os.Getenv("WEB3_PRACTICE_PRIVATE_KEY")
+    privateKey, err := crypto.HexToECDSA(pk)
+	if err != nil {
+ 		log.Fatal(err)
+	}
+
+    publicKey := privateKey.Public()
+    publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+    if !ok {
+        log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+    }
+
+    fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+    nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    chainID, err := client.NetworkID(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+    auth.Nonce = big.NewInt(int64(nonce))
+    auth.Value = big.NewInt(0)     // in wei
+    auth.GasLimit = uint64(300000) // in units
+    auth.GasPrice = gasPrice
+
+    key := [32]byte{}
+    value := [32]byte{}
+    copy(key[:], []byte("test15"))
+    copy(value[:], []byte("burrrrr"))
+
+    tx, err := instance.SetItem(auth, key, value)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("tx sent: %s\n", tx.Hash().Hex()) 
+
+    time.Sleep(35 * time.Second) // time is needed for the contract to update onchain, but no need to reload the instance
+
+    result, err := instance.Items(nil, key)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+
+    fmt.Printf("The result is: %s\n",string(result[:])) // burrrrr
 }
