7fd53d3c6a7616ca43c61e4aceebe952b8c69601
Xiangyu Xu <xuxiangy@amazon.com>
2023-12-28 10:58:22 -0800

    create an interface for ERC20's events && relevant builds

--- contracts/contract_erc20_events/erc20_events.sol
+++ contracts/contract_erc20_events/erc20_events.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract ERC20 {
+contract ERC20EVENTS {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
