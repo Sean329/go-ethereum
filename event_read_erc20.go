58569547d961a60479d2c8b1ac94b03cfd5e6e0a
Xiangyu Xu <xuxiangy@amazon.com>
2023-12-29 11:24:26 -0800

    Finished event_read_erc20.go

--- event_read_erc20.go
+++ event_read_erc20.go
@@ -69,5 +69,47 @@ func main() {
 	logApprovalSig := []byte("Approval(address,address,uint256)")
 	logTransferSigHash := crypto.Keccak256Hash(logTransferSig)
 	logApprovalSigHash := crypto.Keccak256Hash(logApprovalSig)
-    
+
+	for _, vLog := range logs {
+        fmt.Printf("Log Block Number: %d\n", vLog.BlockNumber)
+        fmt.Printf("Log Index: %d\n", vLog.Index)
+
+        switch vLog.Topics[0].Hex() {
+        case logTransferSigHash.Hex():
+            fmt.Printf("Log Name: Transfer\n")
+
+            var transferEvent LogTransfer
+
+            err := contractAbi.UnpackIntoInterface(&transferEvent, "Transfer", vLog.Data)
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            transferEvent.From = common.HexToAddress(vLog.Topics[1].Hex())
+            transferEvent.To = common.HexToAddress(vLog.Topics[2].Hex())
+
+            fmt.Printf("From: %s\n", transferEvent.From.Hex())
+            fmt.Printf("To: %s\n", transferEvent.To.Hex())
+            fmt.Printf("Tokens: %s\n", transferEvent.Tokens.String())
+
+        case logApprovalSigHash.Hex():
+            fmt.Printf("Log Name: Approval\n")
+
+            var approvalEvent LogApproval
+
+            err := contractAbi.UnpackIntoInterface(&approvalEvent, "Approval", vLog.Data)
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            approvalEvent.TokenOwner = common.HexToAddress(vLog.Topics[1].Hex())
+            approvalEvent.Spender = common.HexToAddress(vLog.Topics[2].Hex())
+
+            fmt.Printf("Token Owner: %s\n", approvalEvent.TokenOwner.Hex())
+            fmt.Printf("Spender: %s\n", approvalEvent.Spender.Hex())
+            fmt.Printf("Tokens: %s\n", approvalEvent.Tokens.String())
+        }
+
+        fmt.Printf("\n\n")
+    }
 }
